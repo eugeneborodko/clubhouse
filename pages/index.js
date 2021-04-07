c271694aaab1aeaf374d8fca7bd9fdb1585a5d42
evgenborod496 <eugeneborodko@gmail.com>
2021-04-07 13:06:02 +0300

    init: start the project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Clubhouse: Drop-in audio chat</title>
+      </Head>
+      <h1>Home Page</h1>
+    </>
+  )
+}
+
+export default Home
