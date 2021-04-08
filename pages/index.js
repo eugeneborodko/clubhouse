cab179b5788654d265d1a84ee2941ae045d85778
evgenborod496 <eugeneborodko@gmail.com>
2021-04-08 11:34:02 +0300

    feat: add twitter import popup

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
 import { useState } from 'react'
-import Container from './../components/Container'
 import Greeting from './../components/Steps/Greeting'
 import EnterName from './../components/Steps/EnterName'
+import TwitterImport from './../components/Steps/TwitterImport'
 
 const stepsComponents = {
   0: Greeting,
   1: EnterName,
+  2: TwitterImport,
 }
 
 const Home = () => {
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(2)
   const Step = stepsComponents[step]
 
   return (
@@ -18,9 +19,7 @@ const Home = () => {
       <Head>
         <title>Clubhouse: Drop-in audio chat</title>
       </Head>
-      <Container>
-        <Step />
-      </Container>
+      <Step />
     </>
   )
 }
