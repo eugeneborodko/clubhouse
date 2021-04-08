c493af2f49ecc916d06e9a75eb2267684db6dde9
evgenborod496 <eugeneborodko@gmail.com>
2021-04-08 15:29:37 +0300

    feat: add transition to rooms page

---
+++ pages/rooms.js
@@ -0,0 +1,11 @@
+import Container from './../components/Container'
+
+const Rooms = () => {
+  return (
+    <Container>
+      <h1>This is Rooms Page</h1>
+    </Container>
+  )
+}
+
+export default Rooms
